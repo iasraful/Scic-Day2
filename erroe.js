987dcd15009812a50b0585fe3be67fb8bd591772
Asraful Islam <iasraful321@gmail.com>
2021-05-16 21:27:29 +0600

    today

--- erroe.js
+++ erroe.js
@@ -1,10 +1,35 @@
-function check() {
+// function check() {
             
         
-    try{
+//     try{
         
-        alert('Your code is ready to go')
+//         alert('Your code is ready to go')
 
-    }catch(err){
-        alert('You did ana Error On Code')
-    }}
+//     }catch(err){
+//         alert('You did ana Error On Code')
+//     }}
+
+//     if (
+//         id === 123 &&
+//         moonPhase === 'Waning Gibbous' &&
+//         zodiacSign === 'Libra'
+//       ) {
+//         letTheSorceryBegin();
+//       }
+      
+
+
+      // This code will do this thing (...) and that thing (...)
+// ...and who knows what else...
+
+
+// Day 4
+
+const name = "Arian";
+
+if (name.length > 0) {
+  console.log("condition is True");
+  
+}else{
+  console.log("Condition is false");
+}
